875d48e95db667ee7459c5c5bb55abc92c670a54
Ornob Singh <ornobsingh14@gmail.com>
2026-02-17 13:44:20 +0600

    create banner for book vibe

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router";
+
+const Navbar = () => {
+  const link = (
+    <>
+      <NavLink className="p-2 font-semibold">Home</NavLink>
+      <NavLink className="p-2 font-semibold">Listed Books</NavLink>
+      <NavLink className="p-2 font-semibold">Pages to Read</NavLink>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {link}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-2xl font-bold">Book Vibe</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{link}</ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn bg-[#23BE0A] text-white mr-2">Sign In</button>
+        <button className="btn bg-[#59C6D2] text-white">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
